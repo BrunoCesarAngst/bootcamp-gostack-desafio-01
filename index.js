97b0c1f3e21fc5935c4ec2336e9b1c214f6c9f49
brunocesarangst <bruno.angst@rede.ulbra.br>
2019-12-03 21:45:52 -0300

    feature method post

--- index.js
+++ index.js
@@ -21,4 +21,19 @@ server.get('/projects', (req, res) => {
   return res.json(projects);
 });
 
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
+})
+
+
 server.listen(4000);
