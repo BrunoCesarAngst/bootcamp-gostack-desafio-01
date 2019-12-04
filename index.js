5f59e0cde2aa9ea7ce5bd6f13c719d05724ae978
brunocesarangst <bruno.angst@rede.ulbra.br>
2019-12-03 21:43:02 -0300

    feature array and method get

--- index.js
+++ index.js
@@ -2,4 +2,23 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
+const projects = [
+  {
+    id: "0",
+    title: 'Projeto',
+    tasks: [
+      "tarefa"
+    ]
+  }
+]
+
+/**
+ * Mostra todos os projetos.
+ */
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
 server.listen(4000);
